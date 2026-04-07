9ce04b85c33c880e159a9368f9458327f8c7f3ab
iampraiez <petolulope7@gmail.com>
2026-04-07 23:30:54 +0100

    refactor: migrate project structure to src directory and reorganize core services, components, and API routes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./drizzle",
-  schema: "./drizzle/db/schema.ts",
+  out: "./src/drizzle",
+  schema: "./src/drizzle/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
